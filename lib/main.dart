89d4817e6fc5a43abce2c6256d91eb5aaf7175ba
masa-futa <masakunn1215@gmail.com>
2021-08-31 22:53:08 +0900

    [Add] カメラ機能とフォトライブラリアクセス

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  final picker = ImagePicker();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("カメラ"),
+        ),
+        body: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RaisedButton(
+                  child: Text("カメラ起動"),
+                  onPressed: () {
+                    getImage();
+                  },
+                ),
+                RaisedButton(
+                  child: Text("ライブラリ起動"),
+                  onPressed: () {
+                    // _getPhoto();
+                    loadAssets();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+  }
+  /// 1枚のみ取得時
+  Future _getPhoto() async {
+    var image = await picker.pickImage(source: ImageSource.gallery);
+  }
+
+  List<Asset> images = <Asset>[];
+
+  /// 複数取得時
+  Future<void> loadAssets() async {
+    List<Asset> resultList = <Asset>[];
+    String error = 'No Error Detected';
+    try {
+      resultList = await MultiImagePicker.pickImages(
+        maxImages: 300,
+        enableCamera: true,
+        selectedAssets: images,
+        cupertinoOptions: CupertinoOptions(takePhotoIcon: "chat"),
+        materialOptions: MaterialOptions(
+          actionBarColor: "#abcdef",
+          actionBarTitle: "Example App",
+          allViewTitle: "All Photos",
+          useDetailsView: false,
+          selectCircleStrokeColor: "#000000",
+        ),
+      );
+    } on Exception catch (e) {
+      error = e.toString();
+    }
+  }
+}
